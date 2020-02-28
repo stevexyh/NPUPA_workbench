187a4a4ebe4bb0d2291db0a62c01f8907221f1f1
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-02-29 00:18:20 +0800

    first commit, 完成了下载功能及上传页面

---
+++ downloader/urls.py
@@ -0,0 +1,27 @@
+"""NPUPA_downloader URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from . import views
+
+app_name = 'downloader'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('base/', views.base, name='base'),
+    path('upload/', views.upload, name='upload'),
+    path('downloads/', views.downloads, name='downloads'),
+    path('contents/', views.download_file, name='contents'),
+	# url(r'^download/',views.download,name="download"),
+]
