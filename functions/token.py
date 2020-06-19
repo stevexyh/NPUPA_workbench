53a6b779352c9f4f40aee4f4b9c8a9a29487f1c7
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-06-19 13:14:30 +0800

    feat(weather): Add weather API

---
+++ functions/token.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+* File Name    : token.py
+* Description  : Get token from files
+* Create Time  : 2020-03-13 12:18:29
+* Version      : 1.0
+* Author       : Steve X
+* GitHub       : https://github.com/Steve-Xyh
+'''
+
+
+import configparser
+
+
+TOKEN_FILE = './token'
+WEATHER_KEY = './weather_key'
+INIT_CONF = '.init.conf'
+
+conf = configparser.ConfigParser()
+conf.read(INIT_CONF)
+
+
+def get_token():
+    '''Get token from files'''
+    token = ''
+    with open(TOKEN_FILE, 'r') as f:
+        token = f.readline().strip()
+
+    return token
+
+
+def get_weather_key():
+    '''Get weather api key from files'''
+    token = conf.get('weather', 'key')
+
+    return token
